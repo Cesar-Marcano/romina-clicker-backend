c03bc29039dde8288dde7ac58c85bb8b6ed0371b
Cesar-Marcano <marcanocesar86@gmail.com>
2025-01-09 18:23:03 -0400

    Add ScoreModule to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ScoreModule } from './score/score.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UsersModule } from './users/users.module';
     MongooseModule.forRoot(process.env.MONGODB_URI),
     AuthModule,
     UsersModule,
+    ScoreModule,
   ],
 })
 export class AppModule {}
